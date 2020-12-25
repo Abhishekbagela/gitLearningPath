0a25f0daf77b2e84a1af2682b826453c218e3dfd
Abhishek Bagela <abhibagela2015@gmail.com>
2020-12-25 10:15:49 +0000

    modified Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public Class Hello{
    static{
-     System.out.println("Hello, From Hello");
+     System.out.println("Hello, From Hello 1");
+     System.out.println("Hello, From Hello 2");
    }
 }
